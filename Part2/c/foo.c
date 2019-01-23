357f18bf6bef2a65506cf5f8b24c999c53b86210
Stineaak <46577497+Stineaak@users.noreply.github.com>
2019-01-23 23:58:33 +0100

    Ferdig og testet

--- Part2/c/foo.c
+++ Part2/c/foo.c
@@ -2,12 +2,15 @@
 #include <stdio.h>
 
 int i = 0;
+pthread_mutex_t lock; // lagt til av Stine
 
 // Note the return type: void*
 void* incrementingThreadFunction(){
     for (int j = 0; j < 1000000; j++) {
 	// TODO: sync access to i
+	pthread_mutex_lock(&lock); // lagt til av Stine
 	i++;
+	pthread_mutex_unlock(&lock); // lagt til av Stine
     }
     return NULL;
 }
@@ -15,13 +18,16 @@ void* incrementingThreadFunction(){
 void* decrementingThreadFunction(){
     for (int j = 0; j < 1000000; j++) {
 	// TODO: sync access to i
+	pthread_mutex_lock(&lock); // lagt til av Stine
 	i--;
+	pthread_mutex_unlock(&lock); // lagt til av Stine
     }
     return NULL;
 }
 
 
 int main(){
+	pthread_mutex_init(&lock, NULL); // lagt til av Stine
     pthread_t incrementingThread, decrementingThread;
     
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
@@ -33,3 +39,5 @@ int main(){
     printf("The magic number is: %d\n", i);
     return 0;
 }
+
+// koden er testet og fungerer :)
