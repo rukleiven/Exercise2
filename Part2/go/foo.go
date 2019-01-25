6e8e147d1061fe6a888c6ef9321d36765b6677be
Plass 4 sanntid <plass4@sanntidssal.ntnu.no>
2019-01-25 09:33:49 +0100

    Update go file

--- Part2/go/foo.go
+++ Part2/go/foo.go
@@ -20,16 +20,26 @@ func number_server(add_number <-chan int, control <-chan int, number chan<- int)
 	for {
 		select {
 			// TODO: receive different messages and handle them correctly
+    case  add:=<- add_number:
+      i += add
 			// You will at least need to update the number and handle control signals.
+    case check_signal := <- control:
+      if check_signal == GetNumber{
+        number<-i
+      }else if check_signal == Exit{
+        return
+      }
 		}
 	}
 }
 
 func incrementing(add_number chan<-int, finished chan<- bool) {
+
 	for j := 0; j<1000000; j++ {
 		add_number <- 1
 	}
 	//TODO: signal that the goroutine is finished
+  finished <- true
 }
 
 func decrementing(add_number chan<- int, finished chan<- bool) {
@@ -37,17 +47,28 @@ func decrementing(add_number chan<- int, finished chan<- bool) {
 		add_number <- -1
 	}
 	//TODO: signal that the goroutine is finished
+  finished <- true
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	// TODO: Construct the required channels
+  add_number := make(chan int)
+  finished := make(chan bool,2)
+  control := make(chan int)
+  number := make(chan int)
 	// Think about wether the receptions of the number should be unbuffered, or buffered with a fixed queue size.
 
 	// TODO: Spawn the required goroutines
-
+  go number_server(add_number, control, number)
+  go incrementing(add_number, finished)
+  go decrementing(add_number, finished)
 	// TODO: block on finished from both "worker" goroutines
+
+  for i := 0; i<2; i++{
+    <-finished
+  }
 
 	control<-GetNumber
 	Println("The magic number is:", <- number)
